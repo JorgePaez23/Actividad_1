175f54abd4ffed6b16a53e8131ffc595ac43ca02
JorgePaez23 <jpaezleon19@gmail.com>
2024-07-20 23:03:30 -0400

    Add files via upload
    Tarea Tipos de Datos

---
+++ seccion-4/app.js
@@ -0,0 +1,20 @@
+
+let nombre = 'Jorge';
+let apellido = 'Paez';
+
+
+let nombreCompleto = nombre + ' ' + apellido
+
+
+console.log(nombreCompleto);
+
+
+let a = 30;
+let b = 25;
+
+console.log(a + b);
+
+
+let activo = true;
+let despedido = false;
+
