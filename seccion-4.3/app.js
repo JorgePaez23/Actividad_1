d06c7da239169f989b7cdb23bb30eb99717b5c04
JorgePaez23 <jpaezleon19@gmail.com>
2024-07-21 09:59:45 -0400

    Tarea Switch
    Switch

---
+++ seccion-4.3/app.js
@@ -0,0 +1,28 @@
+let dia = 7;
+
+switch ( dia ){
+
+    case 0:
+        console.log('Es Domingo');
+        break;
+    case 1:
+        console.log('Es Lunes') 
+        break;
+    case 2:
+        console.log('Es Martes')
+        break;
+    case 3:
+        console.log('Es Miercoles')
+        break;
+    case 4:
+        console.log('Es Jueves')
+        break;
+    case 5:
+        console.log('Es Viernes')
+        break;
+    case 6:
+        console.log('Es Sabado')
+        break;
+    default:
+        console.log('El dia ingresado no es permitido')
+}
