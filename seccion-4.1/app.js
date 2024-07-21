9a141eda37a8354191e28e835d5406877fdc8372
JorgePaez23 <jpaezleon19@gmail.com>
2024-07-21 09:25:01 -0400

    Estructura de Control
    Tarea Estructura de Control

---
+++ seccion-4.1/app.js
@@ -0,0 +1,12 @@
+
+let nota = 11;
+
+if (nota >= 61){
+    console.log("El alumno aprobo la clase")
+} else if( nota < 50){
+        console.log("ESTUDIAR PARA LA PROXIMA")
+    } else {
+        console.log("El alumno No aprueba la clase")
+    }  
+
+console.log("Fin del programa")
