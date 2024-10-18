023f43df71edac0913a107fa786d590fd5c7e967
JorgePaez23 <jpaezleon19@gmail.com>
2024-10-18 02:02:18 +0200

    Add files via upload

---
+++ seccion-5.4/app.js
@@ -0,0 +1,8 @@
+let personas = ['Moises', 'Esteban', 'Richard' ];
+let salarios = [1000, 1200, 3000];
+
+for(let i = 0; i< personas.length; i++) {
+
+    console.log(personas[i] + 'tiene salario de ' + salarios[i]);
+}
+
